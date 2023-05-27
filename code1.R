843f29eb2be7c63df7a2c94a70ec27ca586e9732
sivakv2 <sivakv@hotmail.com>
2023-05-27 21:03:36 +0530

    code for programming and function

---
+++ code1.R
@@ -0,0 +1,38 @@
+
+#Cuberoot problem
+
+# problem is 
+# take a number betweeen 1 to 10
+
+# mutliply the number by itself
+# mutilply by 5
+# multiply by 2
+# again mutliply the number by itself
+# add 5
+# add 2
+
+*n * n * 5 * 2 * n + 5 + 2
+
+#n^3 * 10 + 7
+
+number =  value ^3 / 10 - 7
+
+value <- 64
+
+value^(1/3)
+
+3^3
+
+27^(1/3)
+
+
+#write function
+
+rv <- function (x){
+  x = x ^ (1/3)
+  return(x)
+}
+
+
+
+
